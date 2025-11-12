94b04742fb8db1e8a55875de4eccd42732a140f2
alexvanos <b137053@atlascollege.nl>
2025-11-12 12:33:17 +0100

    Add files via upload

---
+++ spel-doner.py
@@ -0,0 +1,7 @@
+while True:
+        if states.locatie == "HUIS" :
+            huis.begin()
+        elif states.locatie == "GANG" :
+            gang.begin()
+        elif states.locatie == "ZOLDER" :
+            zolder.begin()
